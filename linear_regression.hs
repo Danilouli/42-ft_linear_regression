fa8df15f958d15af7716cae311fe5dd60ccfc402
Daniel Saadia <daniel@les-sherpas.co>
2020-02-06 18:20:54 +0100

    visu

---
+++ linear_regression.hs
@@ -0,0 +1,55 @@
+-- ************************************************************************** --
+--                                                                            --
+--                                                        :::      ::::::::   --
+--   linear_regression.hs                               :+:      :+:    :+:   --
+--                                                    +:+ +:+         +:+     --
+--   By: daniel <daniel@student.42.fr>              +#+  +:+       +#+        --
+--                                                +#+#+#+#+#+   +#+           --
+--   Created: 2020/01/30 18:42:41 by danilouli         #+#    #+#             --
+--   Updated: 2020/02/06 15:20:33 by daniel           ###   ########.fr       --
+--                                                                            --
+-- ************************************************************************** --
+
+import Text.CSV
+import Text.Read
+
+parseData :: [Record] -> [[Float]]
+parseData [] = []
+parseData xs = map parseCouple xs
+
+toFloat :: String -> Float
+toFloat = read
+parseCouple [a,b] = [toFloat a, toFloat b]
+
+splitData :: [[Float]] -> ([Float],[Float])
+splitData xs = (map (\xxs -> xxs!!0) xs, map (\xxs -> xxs!!1) xs) 
+
+mean :: [Float] -> Float
+mean xs = (sum xs)/(fromIntegral(length xs))
+
+covariance :: ([Float], [Float]) -> Float
+covariance (xs,ys) = sum [(x - x_)*(y - y_) | x <- xs, y <- ys]
+    where x_ = mean xs
+          y_ = mean ys
+
+variance :: [Float] -> Float
+variance xs = covariance(xs,xs)
+
+correlation :: ([Float], [Float]) -> Float
+correlation (xs,ys) = covariance(xs,ys)/variance(xs)
+
+interecept :: ([Float], [Float]) -> Float
+interecept (xs,ys) = (mean ys) - (correlation(xs,ys) * (mean ys))
+
+thetaCouple :: ([Float], [Float]) -> (Float,Float)
+thetaCouple (xs,ys) = (interecept(xs,ys), correlation(xs,ys))
+
+handleError csv = putStrLn "error parsing"
+doWork csv = (print.thetaCouple.splitData.parseData.tail) csv
+
+main :: IO ()
+main = do
+    let fileName = "data.csv"
+    input <- readFile fileName
+    let csv = parseCSV fileName input
+    either handleError doWork csv
